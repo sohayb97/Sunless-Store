135d9e252e419b2a225e58c374a161c48f28ecad
sohayb ahmed <engsohayb97@gmail.com>
2024-05-14 00:18:15 +0300

    new

--- src/app/store-route.module.ts
+++ src/app/store-route.module.ts
@@ -13,9 +13,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports:[RouterModule.forRoot(routes)],
+  imports:[RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
   exports:[RouterModule]
 })
 export class StoreRoutingModule{
-
 }
